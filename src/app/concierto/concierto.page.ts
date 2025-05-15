78c1ec725e27c49b9dcbfb3ae875e150e2dc98bb
Juan Felipe Bahamon Castillo <juan_bahamonca@fet.edu.co>
2025-05-15 16:55:00 -0500

    Concierto :)

--- src/app/concierto/concierto.page.ts
+++ src/app/concierto/concierto.page.ts
@@ -1,3 +1,4 @@
+import { CartService } from './../services/cart.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -7,7 +8,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ConciertoPage implements OnInit {
 
+  concierto: any[] = [];
+
   constructor() { }
+  agregar(precio_concierto: any) {
+    this.concierto.push({ precio_concierto });
+    alert('Precio Agregado');
+  }
+
+  eliminar(precio_concierto: any) {
+    const index = this.concierto.indexOf(precio_concierto);
+    if (index > -1) {
+      this.concierto.splice(index, 1);
+      alert('Precio Eliminado');
+    }
+  }
 
   ngOnInit() {
   }
